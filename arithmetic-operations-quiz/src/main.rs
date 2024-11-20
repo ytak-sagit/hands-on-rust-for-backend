dcc859f4e1d7069ac4310ac69c094fbfe6b91677
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-20 15:22:03 +0000

    feat: `loop`によって正解するまで同じ形式の問題を出し続けるように対応

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -15,32 +15,35 @@ fn main() {
             _ => unreachable!(),
         };
 
-        let op1 = rand::thread_rng().gen_range(0..100);
-        let op2 = rand::thread_rng().gen_range(0..100);
+        loop {
+            let op1 = rand::thread_rng().gen_range(0..100);
+            let op2 = rand::thread_rng().gen_range(0..100);
 
-        println!(
-            "{} {} {} = ??",
-            op1,
-            if is_addition { "+" } else { "-" }, // NOTE: 三項演算子がないので if-else で書く
-            op2
-        );
-        println!("?? の値を入力してください:");
+            println!(
+                "{} {} {} = ??",
+                op1,
+                if is_addition { "+" } else { "-" }, // NOTE: 三項演算子がないので if-else で書く
+                op2
+            );
+            println!("?? の値を入力してください:");
 
-        // ユーザーからの回答を保持する変数
-        let mut ans_input = String::new();
+            // ユーザーからの回答を保持する変数
+            let mut ans_input = String::new();
 
-        // 標準入力から1行取得し、ans_input に代入する
-        std::io::stdin().read_line(&mut ans_input).unwrap();
+            // 標準入力から1行取得し、ans_input に代入する
+            std::io::stdin().read_line(&mut ans_input).unwrap();
 
-        // ans_input から trim() で改行を除去し、parse() で符号あり整数（i32）型に変換する
-        // NOTE: 同名の変数を定義できる（シャドーイング）
-        let ans_input = ans_input.trim().parse::<i32>().unwrap();
+            // ans_input から trim() で改行を除去し、parse() で符号あり整数（i32）型に変換する
+            // NOTE: 同名の変数を定義できる（シャドーイング）
+            let ans_input = ans_input.trim().parse::<i32>().unwrap();
 
-        if dbg!(ans_input == if is_addition { op1 + op2 } else { op1 - op2 }) {
-            println!("正解!");
-            num_of_correct += 1; // 正解したら正解数を1増やす
-        } else {
-            println!("不正解!");
+            if dbg!(ans_input == if is_addition { op1 + op2 } else { op1 - op2 }) {
+                println!("正解!");
+                num_of_correct += 1; // 正解したら正解数を1増やす
+                break; // loop から脱出
+            } else {
+                println!("不正解!");
+            }
         }
     }
 
