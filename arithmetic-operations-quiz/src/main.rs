e7d1778a73e5bad460e037f2989713c596d5e859
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-20 14:49:26 +0000

    feat: 減算に対するクイズ回答が行えるように対応

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -8,9 +8,9 @@ fn main() {
     // 標準入力から1行取得し、ans_input に代入する
     std::io::stdin().read_line(&mut ans_input).unwrap();
 
-    // ans_input から trim() で改行を除去し、parse() で整数（u32）型に変換する
+    // ans_input から trim() で改行を除去し、parse() で符号あり整数（i32）型に変換する
     // NOTE: 同名の変数を定義できる（シャドーイング）
-    let ans_input = ans_input.trim().parse::<u32>().unwrap();
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
 
     dbg!(ans_input); // => cargo run した後に入力した値が確認できる
 
@@ -19,4 +19,24 @@ fn main() {
     } else {
         println!("不正解!");
     }
+
+    println!("1 - 4 = ??");
+    println!("?? の値を入力してください:");
+
+    let mut ans_input = String::new();
+
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
+
+    dbg!(ans_input);
+
+    if dbg!(ans_input == 1 - 4) {
+        println!("正解!");
+    } else {
+        println!("不正解!");
+    }
+
+    println!("i32 が扱えるデータ範囲: {} ~ {}", i32::MIN, i32::MAX);
+    println!("u32 が扱えるデータ範囲: {} ~ {}", u32::MIN, u32::MAX);
 }
