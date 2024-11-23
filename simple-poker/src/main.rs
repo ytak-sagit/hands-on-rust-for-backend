a9233e1dd11b2a8d337aba83c4515756c7ee5716
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-23 08:55:46 +0000

    feat: 手札を引けるように対応

--- simple-poker/src/main.rs
+++ simple-poker/src/main.rs
@@ -15,11 +15,11 @@ struct Card {
 }
 
 fn main() {
-    // Vec の用意
+    // トランプの山札用の Vec の用意
     let mut deck = Vec::<Card>::new();
     let suits = [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade];
 
-    // トランプの山札を作成
+    // 山札を作成
     for suit in suits {
         for rank in 1..=13 {
             // 構造体のインスタンスを生成
@@ -34,5 +34,24 @@ fn main() {
     deck.shuffle(&mut rng);
 
     // NOTE: 構造体やコレクションの中身の表示には {:?} or {:#?} を指定する必要がある
-    println!("{:?}", deck);
+    // println!("{:?}", deck);
+
+    // ---
+
+    // 手札用の Vec の用意
+    let mut hands = Vec::<Card>::new();
+
+    // 5枚のカードを引く
+    for _ in 0..5 {
+        hands.push(deck.pop().unwrap());
+    }
+
+    // 手札を rank 順にソート
+    hands.sort_by(|a, b| a.rank.cmp(&b.rank));
+
+    // 手札の表示
+    println!("--- Hand ---");
+    for (i, card) in hands.iter().enumerate() {
+        println!("{}: {:?} {}", i + 1, card.suit, card.rank);
+    }
 }
