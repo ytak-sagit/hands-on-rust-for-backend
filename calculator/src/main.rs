c87594e0043480ba2284c9310e8019a80e2f32cd
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-23 11:57:04 +0000

    feat: ループして計算できるように対応

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,22 +1,29 @@
+use std::io::stdin;
+
 fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+    for line in stdin().lines() {
+        // 一行読み取って空行なら終了
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
 
-    // 入力値を空白で分割
-    let tokens = input.split_whitespace().collect::<Vec<&str>>();
+        // 入力値を空白で分割
+        let tokens = line.split_whitespace().collect::<Vec<&str>>();
 
-    // 式の計算
-    let left = tokens[0].parse::<f64>().unwrap();
-    let operator = tokens[1];
-    let right = tokens[2].parse::<f64>().unwrap();
-    let result = match operator {
-        "+" => left + right,
-        "-" => left - right,
-        "*" => left * right,
-        "/" => left / right,
-        _ => unreachable!(),
-    };
+        // 式の計算
+        let left = tokens[0].parse::<f64>().unwrap();
+        let operator = tokens[1];
+        let right = tokens[2].parse::<f64>().unwrap();
+        let result = match operator {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => unreachable!(),
+        };
 
-    // 計算結果の表示
-    println!("  => {}", result);
+        // 計算結果の表示
+        println!("  => {}", result);
+    }
 }
