6f56cfc0c495c4231c2f2d67909be91bfa33967c
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-23 11:54:06 +0000

    feat: 入力した文字列から式を認識して計算結果を返すように対応

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    // 入力値を空白で分割
+    let tokens = input.split_whitespace().collect::<Vec<&str>>();
+
+    // 式の計算
+    let left = tokens[0].parse::<f64>().unwrap();
+    let operator = tokens[1];
+    let right = tokens[2].parse::<f64>().unwrap();
+    let result = match operator {
+        "+" => left + right,
+        "-" => left - right,
+        "*" => left * right,
+        "/" => left / right,
+        _ => unreachable!(),
+    };
+
+    // 計算結果の表示
+    println!("  => {}", result);
 }
