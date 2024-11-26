641c7d480420d171465237091994146bfeb68a38
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-26 13:23:38 +0000

    feat: 家計簿管理用のサブコマンドI/Fを定義

--- budget-book/src/main.rs
+++ budget-book/src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(version = "1.0")]
+struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// 新しい口座を作る
+    New,
+    /// 口座に入金する
+    Deposit,
+    /// 口座から出金する
+    Withdraw,
+    /// CSV からインポートする
+    Import,
+    /// レポートを出力する
+    Report,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 構造体 App で定義した形のサブコマンドを受け取ることを期待して parse を行う
+    let _args = App::parse();
 }
