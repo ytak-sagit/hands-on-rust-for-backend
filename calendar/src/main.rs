4154079b5cbcda9dcbe4f3464ccb9f2360b638a6
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-12-01 06:22:31 +0000

    test: `rstest`crateによるパラメタライズドテストを追加

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -114,3 +114,53 @@ fn add_schedule(subject: String, start: NaiveDateTime, end: NaiveDateTime) {
     serde_json::to_writer(writer, &calendar).unwrap();
     println!("予定を追加しました。");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::NaiveDate;
+    use rstest::rstest;
+
+    fn naive_date_time(
+        year: i32,
+        month: u32,
+        day: u32,
+        hour: u32,
+        minute: u32,
+        second: u32,
+    ) -> NaiveDateTime {
+        NaiveDate::from_ymd_opt(year, month, day)
+            .unwrap()
+            .and_hms_opt(hour, minute, second)
+            .unwrap()
+    }
+
+    #[rstest]
+    #[case(18, 15, 18, 45, false)]
+    #[case(18, 15, 19, 15, true)]
+    #[case(18, 30, 20, 15, true)]
+    #[case(19, 15, 19, 45, true)]
+    #[case(19, 45, 20, 45, true)]
+    #[case(20, 15, 20, 45, false)]
+    fn test_schedule_intersects(
+        #[case] h0: u32,
+        #[case] m0: u32,
+        #[case] h1: u32,
+        #[case] m1: u32,
+        #[case] should_intersect: bool,
+    ) {
+        let schedule = Schedule {
+            id: 0,
+            subject: "既存予定".to_string(),
+            start: naive_date_time(2024, 1, 1, h0, m0, 0),
+            end: naive_date_time(2024, 1, 1, h1, m1, 0),
+        };
+        let new_schedule = Schedule {
+            id: 999,
+            subject: "新規予定".to_string(),
+            start: naive_date_time(2024, 1, 1, 19, 0, 0),
+            end: naive_date_time(2024, 1, 1, 20, 0, 0),
+        };
+        assert_eq!(should_intersect, schedule.intersects(&new_schedule));
+    }
+}
