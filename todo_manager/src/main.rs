05c3185c6cfb6299769f6539c2075478d7734cce
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-12-01 12:36:44 +0000

    feat: add API, GET /hello/{name}

--- todo_manager/src/main.rs
+++ todo_manager/src/main.rs
@@ -1,8 +1,8 @@
-use actix_web::{get, App, HttpServer};
+use actix_web::{get, web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_hello))
+    HttpServer::new(|| App::new().service(get_hello).service(get_hello_name))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
@@ -11,4 +11,9 @@ async fn main() -> std::io::Result<()> {
 #[get("/hello")]
 async fn get_hello() -> String {
     "Hello, world!".to_string()
+}
+
+#[get("/hello/{name}")]
+async fn get_hello_name(name: web::Path<String>) -> String {
+    format!("Hello, {name}!")
 }
