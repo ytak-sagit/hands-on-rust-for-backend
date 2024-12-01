4fff90a29d9e05e5a7a8bfd8f9f37305683a6655
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-12-01 12:49:27 +0000

    feat: add API, GET / (todo)

--- todo_manager/src/main.rs
+++ todo_manager/src/main.rs
@@ -4,7 +4,7 @@ use askama_actix::TemplateToResponse;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_hello_name))
+    HttpServer::new(|| App::new().service(get_hello_name).service(get_todo))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
@@ -22,4 +22,14 @@ async fn get_hello_name(name: web::Path<String>) -> HttpResponse {
         name: name.into_inner(),
     };
     hello.to_response()
+}
+
+#[derive(Template)]
+#[template(path = "todo.html")]
+struct TodoTemplate {}
+
+#[get("/")]
+async fn get_todo() -> HttpResponse {
+    let todo = TodoTemplate {};
+    todo.to_response()
 }
