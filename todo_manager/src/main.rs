71a1ee3ecaee254d0f13c3290e289e24bf204e59
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-12-01 12:46:03 +0000

    feat: `askama`, `askama_actix`crateを追加、テンプレートからレスポンスを組み立てるように実装

--- todo_manager/src/main.rs
+++ todo_manager/src/main.rs
@@ -1,19 +1,25 @@
-use actix_web::{get, web, App, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer};
+use askama::Template;
+use askama_actix::TemplateToResponse;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_hello).service(get_hello_name))
+    HttpServer::new(|| App::new().service(get_hello_name))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
 
-#[get("/hello")]
-async fn get_hello() -> String {
-    "Hello, world!".to_string()
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
 }
 
 #[get("/hello/{name}")]
-async fn get_hello_name(name: web::Path<String>) -> String {
-    format!("Hello, {name}!")
+async fn get_hello_name(name: web::Path<String>) -> HttpResponse {
+    let hello = HelloTemplate {
+        name: name.into_inner(),
+    };
+    hello.to_response()
 }
